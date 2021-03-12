27998ff8146e0983b380a9cff3b365cc34d5bc35
Romulo Martinez <mromulo07@gmail.com>
2021-03-12 16:41:32 -0430

    creacion de nueva ruta y view

---
+++ resources/views/romulo.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Soy Romulo </title>
+</head>
+<body>
+<h1>Hola soy Romulo </h1>
+<p> Este es mi primer proyecto laravel </p>
+</body>
+</html>
+</body>    
+</html>
